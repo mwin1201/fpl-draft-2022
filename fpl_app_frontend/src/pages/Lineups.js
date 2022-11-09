48af22ec742a41bce9340f3af42b1f7338f6f66f
Maxwell Winter <mwinter@3PLAY-ML-MWinter.local>
2022-11-09 15:32:02 -0600

    added Lineup page

--- fpl_app_frontend/src/pages/Lineups.js
+++ fpl_app_frontend/src/pages/Lineups.js
@@ -4,18 +4,31 @@ const axios = require('axios').default;
 
 const Lineups = () => {
 
+    const [teamLineups, setTeamLineups] = useState([]);
+    const [players, setPlayers] = useState([]);
+
+
     const location = useLocation();
-    console.log(location.state.data);
+    const leagueTeams = location.state.data;
 
     useEffect(() => {
         getLineups(173696,15);
-    })
+        getPlayers();
+    },[])
 
     const getLineups = (team, gameweek) => {
         axios.get("http://localhost:5000/getLineups/" + team + "/" + gameweek)
         .then((apiResponse) => {
-            console.log("response",apiResponse);
+            setTeamLineups(apiResponse.data.picks);
         })
+    };
+
+    const getPlayers = () => {
+        axios.get("http://localhost:5000/getPremPlayers")
+            .then((apiResponse) => {
+                console.log(apiResponse.data.elements);
+                setPlayers(apiResponse.data.elements);
+            })
     };
 
     return (
@@ -26,13 +39,25 @@ const Lineups = () => {
             <h3>
                 Max's Teams
             </h3>
-            <div>
-                {location.state.data.map((team) => (
-                        <div key={team.id}>
+                {players.filter((player) => (
+                    teamLineups.find((plyer) => player.id === plyer.element)
+                ))
+                .map((filteredPlayer,i) => (
+                    <div key={filteredPlayer.id}>
+                        {filteredPlayer.first_name} {filteredPlayer.second_name} {filteredPlayer.total_points}
+                    </div>
+                ))}
+                <h3>
+                    Teams
+                </h3>
+                <div>
+                    {leagueTeams.map((team) => (
+                        <div key={team.entry_id}>
                             {team.entry_name}
                         </div>
                     ))}
-            </div>
+                </div>
+
         </div>
     )
 
