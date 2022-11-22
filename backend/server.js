7ee410fdc4a432e19290aaf778408f30b7ae4b87
Maxwell Winter <mwinter@3PLAY-ML-MWinter.local>
2022-11-22 09:55:56 -0600

    added package json to root

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const fetch = require("node-fetch");
 const bodyParser = require("body-parser");
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(bodyParser.json());
