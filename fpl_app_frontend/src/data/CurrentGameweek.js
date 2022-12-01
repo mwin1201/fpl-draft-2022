53693f5ce3d2dc88e70b4f702cd52756a515a94e
Maxwell Winter <mwinter@3PLAY-ML-MWinter.local>
2022-12-01 17:59:17 -0600

    env variable setting

--- fpl_app_frontend/src/data/CurrentGameweek.js
+++ fpl_app_frontend/src/data/CurrentGameweek.js
@@ -1,7 +1,7 @@
 const axios = require("axios").default;
 
 const getGameweek = () => {
-    let currentOrigin = process.env.prodOrigin ? process.env.NODE_ENV == 'production' : "http://localhost:5000"
+    let currentOrigin = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_prodOrigin : "http://localhost:5000";
     axios.get(`${currentOrigin}/getGameweek`)
         .then((apiResponse) => {
             localStorage.setItem("current_gameweek", JSON.stringify(apiResponse.data.current_event));
--- fpl_app_frontend/src/data/DraftData.js
+++ fpl_app_frontend/src/data/DraftData.js
@@ -1,7 +1,7 @@
 const axios = require("axios").default;
 
 const getDraftData = () => {
-    let currentOrigin = process.env.prodOrigin ? process.env.NODE_ENV == 'production' : "http://localhost:5000"
+    let currentOrigin = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_prodOrigin : "http://localhost:5000";
     axios.get(`${currentOrigin}/getDraftData`)
         .then((apiResponse) => {
             localStorage.setItem("draft_data", JSON.stringify(apiResponse.data.choices));
