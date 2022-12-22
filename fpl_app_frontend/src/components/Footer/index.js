eb8224d5e0854604753ee4ba334e8e6f8478778b
Maxwell Winter <mwinter@3PLAY-ML-MWinter.local>
2022-12-22 09:37:53 -0600

    added header and footer

---
+++ fpl_app_frontend/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                &copy;2022 Made by football fans, for football fans.
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
--- fpl_app_frontend/src/pages/Lineups.js
+++ fpl_app_frontend/src/pages/Lineups.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import {Link, useNavigate} from "react-router-dom"
 const axios = require('axios').default;
 
 const Lineups = () => {
@@ -9,11 +8,6 @@ const Lineups = () => {
 
     let players = JSON.parse(localStorage.getItem("elements"));
     let leagueTeams = JSON.parse(localStorage.getItem("league_entries"));
-
-    const navigate = useNavigate();
-    const goToHomepage = () => {
-        navigate("/");
-    };
 
     const getLineups = (team, gameweek) => {
         let currentOrigin = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_prodOrigin : "http://localhost:5000";
@@ -34,12 +28,6 @@ const Lineups = () => {
 
     return (
         <div>
-            <div>
-                <Link to="/"></Link>
-                <button onClick={goToHomepage}>
-                    Homepage
-                </button>
-            </div>
             <div>
                 <h2>
                     Filters
