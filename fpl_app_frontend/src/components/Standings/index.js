a99f7b8f0af9f92b29a10a384f3e51a6514a58f5
Max Winter <mwinter@wesleyan.edu>
2023-07-05 20:20:32 -0500

    added new standings component

---
+++ fpl_app_frontend/src/components/Standings/index.js
@@ -0,0 +1,54 @@
+import React, {useState, useEffect} from 'react';
+
+const Standings = ({standings, teams}) => {
+    const [standingsData, setStandingsData] = useState([standings]);
+    const [teamData, setTeamData] = useState([teams]);
+
+    const getEntryName = (entry_id) => {
+        let oneTeam = teamData.filter((team) => {
+            return team.id === entry_id;
+        });
+
+        return oneTeam[0].entry_name;
+    };
+
+    const getDifference = (num1, num2) => {
+        return num1 - num2;
+    };
+
+    return (
+        <section>
+            <h3>League Standings</h3>
+            <table className="table-data">
+                <thead>
+                <tr>
+                    <th>League Team</th>
+                    <th>Wins</th>
+                    <th>Draws</th>
+                    <th>Losses</th>
+                    <th>Pts For</th>
+                    <th>Pts Against</th>
+                    <th>Pts Diff</th>
+                    <th>Total Table Points</th>
+                </tr>
+                </thead>
+                <tbody>
+                {standingsData.map((player) => (
+                    <tr key={player.league_entry}>
+                        <td>{getEntryName(player.league_entry)}</td>
+                        <td>{player.matches_won}</td>
+                        <td>{player.matches_drawn}</td>
+                        <td>{player.matches_lost}</td>
+                        <td>{player.points_for}</td>
+                        <td>{player.points_against}</td>
+                        <td>{getDifference(player.points_for, player.points_against)}</td>
+                        <td>{player.total}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </section>
+    )
+};
+
+export default Standings;
--- fpl_app_frontend/src/pages/Dashboard.js
+++ fpl_app_frontend/src/pages/Dashboard.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import TeamStats from "../components/TeamStats";
+import Standings from "../components/Standings";
 
 const Dashboard = () => {
 
     // Dashboard is only accessible to those who login
     // User, aka Owner, will be able to see:
     // 1. Team Name on Top
-    // 2. Full Stat Card
+    // 2. Full Stat Card - both season card and gameweek card
     // 3. Standings with their specific name highlighted
     // 4. Personal Team Fixture History
     // 5. Wallet Value
@@ -17,6 +18,12 @@ const Dashboard = () => {
         <section>
             <h2>[TEAM NAME]</h2>
             <TeamStats owner_id={217515}/>
+
+            <h2>Standings</h2>
+            <Standings 
+                standings={JSON.parse(localStorage.getItem("standings"))}
+                teams = {JSON.parse(localStorage.getItem("league_entries"))}
+            />
         </section>
     )
 };
