5ca874d02339c0e73196233664c87c96be2c04c4
Max Winter <mwinter@wesleyan.edu>
2023-07-20 19:01:36 -0500

    postgreSQL connection fix

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -3,40 +3,10 @@ const Sequelize = require("sequelize");
 let dbConfig,sequelize;
 
 if (process.env.NODE_ENV === 'production') {
-  dbConfig = {
-    HOST: process.env.intHOST,
-    USER: process.env.intUSER,
-    PASSWORD: process.env.intPASSWORD,
-    DB: process.env.intDB,
-    dialect: "postgres",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
 
   sequelize = new Sequelize(process.env.DB_URI_INTERNAL, {
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: true
-    }
+    dialect: "postgres"
   });
-
-  // sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  //   host: dbConfig.HOST,
-  //   dialect: dbConfig.dialect,
-  //   dialectOptions: {
-  //     ssl: true
-  //   },
-  //   pool: {
-  //       max: dbConfig.pool.max,
-  //       min: dbConfig.pool.min,
-  //       acquire: dbConfig.pool.acquire,
-  //       idle: dbConfig.pool.idle
-  //   }
-  // });
 
 } else {
 
