75cd9a771e887e8c835a44ee01b461a35c64658e
Max Winter <mwinter@wesleyan.edu>
2023-07-24 08:15:17 -0500

    form changes

--- fpl_app_frontend/src/pages/Login.js
+++ fpl_app_frontend/src/pages/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import DangerAlert from '../alerts/FormAlerts/danger';
+import Spinner from 'react-bootstrap/Spinner';
 
 const Login = () => {
     const [formState, setFormState] = useState({
@@ -7,6 +8,7 @@ const Login = () => {
         password: ""
     });
     const [errorMessage, setErrorMessage] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleChange = (event) => {
         const { name, value } = event.target;
@@ -20,29 +22,35 @@ const Login = () => {
     const handleLogin = async (event) => {
         event.preventDefault();
 
+        setIsLoading(true);
+
         const team_name = document.getElementById("team_name").value.trim();
         const password = document.getElementById("password").value.trim();
 
         if (team_name && password) {
             let currentOrigin = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_prodOrigin : "http://localhost:5000";
-            const response = await fetch(`${currentOrigin}/api/owners/login`, {
+            await fetch(`${currentOrigin}/api/owners/login`, {
                 method: "post",
                 body: JSON.stringify(formState),
                 headers: { "Content-Type": "application/json"}
-            })
+            }).then((response) => {
+                if (response.ok) {
+                    return response.json();
+                }
+                else {
+                    throw response;
+                }
+            }).then((data) => {
+                localStorage.setItem("current_user", JSON.stringify(data));
+                setIsLoading(false);
 
-            const data = await response.json();
-            localStorage.setItem("current_user", JSON.stringify(data));
-
-            if (response.ok) {
-                // let appURL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_URL : "http://localhost:3000";
-                // document.location.replace(`${appURL}/dashboard`);
                 document.location.replace("/dashboard");
-            }
-
-            else {
-                setErrorMessage("Either your team name or password is not correct. Try again.");
-            }
+    
+            }).catch((err) => {
+                setIsLoading(false);
+                setErrorMessage("There was an issue with the submission. Please try again.");
+                err.text().then(message => alert(message));
+            });
         }
         else {
             setErrorMessage("Make sure you have both fields populated!");
@@ -51,20 +59,26 @@ const Login = () => {
 
     return (
         <section>
-            <form className='login-form' onSubmit={handleLogin} autoComplete='off'>
-                <h2>Welcome back to FPL Madness</h2>
+            {isLoading ?
+                <div>
+                    <Spinner variant='danger'></Spinner>
+                </div>
+            :
+                <form className='login-form' onSubmit={handleLogin} autoComplete='off'>
+                    <h2>Welcome back to FPL Madness</h2>
 
-                <DangerAlert message={errorMessage} />
+                    <DangerAlert message={errorMessage} />
 
-                <label htmlFor='team_name'>Team Name:</label>
-                <input id="team_name" name="team_name" type='text' onChange={handleChange} />
+                    <label htmlFor='team_name'>Team Name:</label>
+                    <input id="team_name" name="team_name" type='text' onChange={handleChange} />
 
-                <label htmlFor='password'>Password:</label>
-                <input id='password' name="password" type='password' onChange={handleChange} />
+                    <label htmlFor='password'>Password:</label>
+                    <input id='password' name="password" type='password' onChange={handleChange} />
 
-                <button type='submit'>Submit</button>
-                <a href='/update'>Forgot your password?</a>
-            </form>
+                    <button type='submit'>Submit</button>
+                    <a href='/update'>Forgot your password?</a>
+                </form>
+            }
         </section>
     )
 };
--- fpl_app_frontend/src/pages/SignUp.js
+++ fpl_app_frontend/src/pages/SignUp.js
@@ -8,8 +8,8 @@
 import React, { useState } from 'react';
 import DangerAlert from '../alerts/FormAlerts/danger';
 import SuccessAlert from '../alerts/FormAlerts/success';
-// will need to remove comments when league is created
 import getLeagueData from '../data/LeagueData';
+import Spinner from 'react-bootstrap/Spinner';
 
 const SignUp = () => {
     const [formState, setFormState] = useState(
@@ -26,6 +26,8 @@ const SignUp = () => {
     const [success, setSuccess] = useState("");
     const [errorMessage2, setErrorMessage2] = useState("");
     const [success2, setSuccess2] = useState("");
+    const [isSearchLoading, setIsSearchLoading] = useState(false);
+    const [isSubmitLoading, setIsSubmitLoading] = useState(false);
 
     const handleChange = (event) => {
         const { name, value } = event.target;
@@ -36,6 +38,13 @@ const SignUp = () => {
                 ...formState,
                 [name]: primary_league_id
             })
+        }
+        else if (name === "secondary_league_id") {
+            const secondary_league_id = parseInt(value);
+            setFormState({
+                ...formState,
+                [name]: secondary_league_id
+            });
         }
         else {
             setFormState({
@@ -48,6 +57,7 @@ const SignUp = () => {
     const handleOwnerSearch = async (event) => {
         event.preventDefault();
 
+        setIsSearchLoading(true);
         const primary_league_id = document.getElementById("primary_league_id").value;
         const team_name = document.getElementById("team_name").value;
 
@@ -58,9 +68,8 @@ const SignUp = () => {
         ]).then((data) => {
             const leagueTeams = JSON.parse(localStorage.getItem("league_entries"));
             const leaguePlayer = leagueTeams.filter((team) => team.entry_name === team_name);
+            setIsSearchLoading(false);
             let entry_id, fpl_id;
-
-            console.log(leaguePlayer);
 
             if (leaguePlayer.length === 0) {
                 setErrorMessage("Could not find team with provided information");
@@ -88,53 +97,59 @@ const SignUp = () => {
         const team_name = document.getElementById("team_name").value.length;
         const password = document.getElementById("password").value.length;
         const primary_league_id = document.getElementById("primary_league_id").value.length;
+        const secondary_league_id = document.getElementById("secondary_league_id").value.length;
 
         if (!team_name || !primary_league_id) {
-            setErrorMessage("You are missing a required field");
+            setErrorMessage("Your team name and primary league ID are required.");
             setSuccess("");
-        } else if (!password) {
-            setErrorMessage2("Your password is required");
+        } else if (!password || !secondary_league_id) {
+            setErrorMessage2("Your password and secondary league ID are required.");
             setSuccess2("");
         } else {
+            setIsSubmitLoading(true);
             let currentOrigin = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_prodOrigin : "http://localhost:5000";
-            const response = await fetch(`${currentOrigin}/api/owners`, {
+            await fetch(`${currentOrigin}/api/owners`, {
                 method: "post",
                 body: JSON.stringify(formState),
                 headers: { "Content-Type": "application/json"}
-            });
-
-            const data = await response.json();
-            localStorage.setItem("current_user", JSON.stringify(data));
-
-            if (response.ok) {
+            }).then((response) => {    
+                if (response.ok) {
+                    return response.json();
+                }
+                else {
+                    throw response;
+                }
+            }).then((data) => {
+                localStorage.setItem("current_user", JSON.stringify(data));
+                setIsSubmitLoading(false);
                 document.location.replace('/dashboard');
-            }
-            else {
-                alert(response.statusText);
-            }
+            })
+            .catch((err) => {
+                setIsSubmitLoading(false);
+                setErrorMessage2("There was an issue with the submission. Please try again.");
+            });
         }
     };
 
     return (
         <section>
             <form id="signup-search" onSubmit={handleOwnerSearch} autoComplete="off">
-                <div>
-                    <h1>Join in the FPL Fun!</h1>
+                <h1>Join in the FPL Fun!</h1>
 
-                    <h3>First, enter your FPL Draft League ID and your team name in that League.</h3>
-                    <p>This is so we can search for you and pre-populate data relative to your team.</p>
+                <h3>First, enter your FPL Draft League ID and your team name in that League.</h3>
+                <p>This is so we can search for you and pre-populate data relative to your team.</p>
 
-                    <DangerAlert message={errorMessage} />
-                    <SuccessAlert message={success} />
+                <DangerAlert message={errorMessage} />
+                <SuccessAlert message={success} />
+                {isSearchLoading ? <Spinner variant='danger' /> : ""}
 
-                    <label htmlFor="primary_league_id">League ID:</label>
-                    <input id="primary_league_id" name="primary_league_id" type="number" onBlur={handleChange}></input>
+                <label htmlFor="primary_league_id">League ID:</label>
+                <input id="primary_league_id" name="primary_league_id" type="number" onBlur={handleChange}></input>
 
-                    <label htmlFor="team_name">Team Name: </label>
-                    <input id="team_name" name="team_name" type="text" onBlur={handleChange}></input>
+                <label htmlFor="team_name">Team Name: </label>
+                <input id="team_name" name="team_name" type="text" onBlur={handleChange}></input>
 
-                    <button>Search</button>
-                </div>
+                <button>Search</button>
             </form>
 
             <form id='signup-submit' className='hide-form' onSubmit={handleOwnerSignUp} autoComplete="off">
@@ -142,6 +157,7 @@ const SignUp = () => {
                 
                 <DangerAlert message={errorMessage2} />
                 <SuccessAlert message={success2} />
+                {isSubmitLoading ? <Spinner variant='danger' /> : ""}
 
                 <label htmlFor="password">Password:</label>
                 <input id="password" name="password" type="password" onBlur={handleChange}></input>
