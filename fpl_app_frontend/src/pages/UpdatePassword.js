0d211ce01f2145015bf94896f8d3d223287fed94
Max Winter <mwinter@wesleyan.edu>
2023-07-25 09:29:33 -0500

    update changes

--- fpl_app_frontend/src/pages/UpdatePassword.js
+++ fpl_app_frontend/src/pages/UpdatePassword.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import DangerAlert from '../alerts/FormAlerts/danger';
+import Spinner from 'react-bootstrap/Spinner';
 
 const UpdatePassword = () => {
     const [errorMessage, setErrorMessage] = useState("");
@@ -6,6 +8,7 @@ const UpdatePassword = () => {
         team_name:"",
         password: ""
     });
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleChange = (event) => {
         const { name, value } = event.target;
@@ -24,44 +27,55 @@ const UpdatePassword = () => {
         const password = document.getElementById("password").value.trim();
 
         if (team_name && password) {
+            setIsLoading(true);
             let currentOrigin = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_prodOrigin : "http://localhost:5000";
-            const response = await fetch(`${currentOrigin}/api/owners`, {
+            await fetch(`${currentOrigin}/api/owners`, {
                 method: "put",
                 body: JSON.stringify(formState),
                 headers: { "Content-Type": "application/json"}
-            })
-
-            const data = await response.json();
-            const ownerData = data[1][0];
-            localStorage.setItem("current_user", JSON.stringify(ownerData));
-
-            if (response.ok) {
+            }).then((response) => {
+                if (response.ok) {
+                    return response.json();
+                }
+                else {
+                    throw response;
+                }
+            }).then((data) => {
+                const ownerData = data[1][0];
+                localStorage.setItem("current_user", JSON.stringify(ownerData));
+                setIsLoading(false);
                 document.location.replace("/dashboard");
-            }
-
-            else {
-                setErrorMessage("Either your team name or password is not correct. Try again.");
-            }
+            }).catch((err) => {
+                setIsLoading(false);
+                setErrorMessage("Either your team name is not correct or password is not at least 8 characters. Try again.");
+            });
         }
         else {
-            setErrorMessage("Make sure you have both fields populated!");
+            setErrorMessage("Make sure you have both fields populated! Password must be at least 8 characters.");
         }
     };
 
     return (
         <section>
-            <form onSubmit={handleUpdate} autoComplete='off'>
-                <h2>Update password below</h2>
-                {errorMessage && <p>{errorMessage}</p>}
-                
-                <label htmlFor='team_name'>Team Name:</label>
-                <input id="team_name" name="team_name" type='text' onChange={handleChange} />
+            {isLoading ?
+                <div>
+                    <Spinner variant='danger'></Spinner> 
+                </div>
+            :
+                <form id='password-update' onSubmit={handleUpdate} autoComplete='off'>
+                    <h2>Update password below</h2>
+                    
+                    <DangerAlert message={errorMessage} />
+                    
+                    <label htmlFor='team_name'>Team Name:</label>
+                    <input id="team_name" name="team_name" type='text' onChange={handleChange} />
 
-                <label htmlFor='password'>Password:</label>
-                <input id='password' name="password" type='password' onChange={handleChange} />
+                    <label htmlFor='password'>Password:</label>
+                    <input id='password' name="password" type='password' onChange={handleChange} />
 
-                <button type='submit'>Submit</button>
-            </form>
+                    <button type='submit'>Submit</button>
+                </form>
+            }
         </section>
     )
 
