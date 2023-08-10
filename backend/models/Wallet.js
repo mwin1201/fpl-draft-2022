8ec9d3a03d38e2abcb254cc3100bea7fc9cc8f2c
Max Winter <mwinter@wesleyan.edu>
2023-08-10 15:13:34 -0500

    added new tables

---
+++ backend/models/Wallet.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Wallet extends Model {}
+
+Wallet.init (
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        total: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 0,
+            }
+        },
+
+        //foreignKey
+        owner_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "owner",
+                key: "id"
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "wallet"
+    }
+);
+
+module.exports = Wallet;
