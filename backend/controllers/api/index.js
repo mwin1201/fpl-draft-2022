067eff779b90b1c7df97f2e72251c513b1a0a826
Max Winter <mwinter@wesleyan.edu>
2023-08-22 13:52:38 -0500

    adding wallet routes

--- backend/controllers/api/index.js
+++ backend/controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const ownerRoutes = require("./owner-routes");
 const betRoutes = require("./bet-routes");
+const walletRoutes = require("./wallet-bets");
 
 router.use("/owners", ownerRoutes);
 router.use("/bets", betRoutes);
+router.use("/wallets", walletRoutes);
 
 module.exports = router;
