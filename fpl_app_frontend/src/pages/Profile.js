b0196b184a88c7fc060d6f045ff9dd746e5d3ccd
Max Winter <mwinter@wesleyan.edu>
2023-09-14 12:21:28 -0500

    added wallet value

--- fpl_app_frontend/src/pages/Profile.js
+++ fpl_app_frontend/src/pages/Profile.js
@@ -4,6 +4,8 @@ import FixtureHistory from "../components/FixtureHistory";
 import UpcomingFixtures from "../components/UpcomingFixtures";
 import PersonalBets from "../components/Bets";
 import Lineup from "../components/Lineup";
+import TeamStats from "../components/TeamStats";
+import WalletValue from "../components/WalletValue";
 
 const Profile = () => {
     let { id: fpl_id} = useParams();
@@ -16,6 +18,8 @@ const Profile = () => {
                 <h1>Profile for {ownerProfile[0].entry_name}</h1>
                 <img className="avatar" src={ownerProfile[0].avatar} alt="Owner avatar"></img>
 
+                <TeamStats owner_entry_id={ownerProfile[0].entry_id} />
+
                 <Lineup owner_id={ownerProfile[0].entry_id} />
 
                 <h2>Past Fixtures</h2>
@@ -24,6 +28,7 @@ const Profile = () => {
                 <h2>Upcoming Fixtures</h2>
                 <UpcomingFixtures owner_id={fpl_id} />
 
+                <WalletValue owner_id={fpl_id} />
                 <PersonalBets owner_id={fpl_id}/>
             </section>
         </main>
