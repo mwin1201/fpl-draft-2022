a01e74ebda1e5b7434b233e1a3d9cfa52eb94523
Max Winter <mwinter@wesleyan.edu>
2023-11-28 12:36:25 -0600

    minor change

--- fpl_app_frontend/src/pages/ChampionshipPlayoffs.js
+++ fpl_app_frontend/src/pages/ChampionshipPlayoffs.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Spinner from 'react-bootstrap/Spinner';
+import Playoffs from "../components/ChampionshipPlayoffs";
 
 const ChampionshipPlayoffs = () => {
     return (
         <div>
-            <h1>Playoffs!!</h1>
+            <Playoffs league_id={JSON.parse(localStorage.getItem("current_league"))}/>
         </div>
     )
 };
