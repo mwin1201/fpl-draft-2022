35ed45a424eedac2f30c587263e46d3d148666cc
Max Winter <mwinter@wesleyan.edu>
2023-12-12 11:09:32 -0600

    added league data table and associated record changes

--- backend/controllers/api/index.js
+++ backend/controllers/api/index.js
@@ -3,9 +3,11 @@ const router = require("express").Router();
 const ownerRoutes = require("./owner-routes");
 const betRoutes = require("./bet-routes");
 const walletRoutes = require("./wallet-bets");
+const leagueDataRoutes = require("./leagueData-routes");
 
 router.use("/owners", ownerRoutes);
 router.use("/bets", betRoutes);
 router.use("/wallets", walletRoutes);
+router.use("/leaguedata", leagueDataRoutes);
 
 module.exports = router;
